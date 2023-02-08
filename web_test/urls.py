2e618c0af3cdcb7b055fe68b483eed221a6e0df4
Akira Watanabe <a0323w@gmail.com>
2023-02-08 18:45:06 +0900

    日付の確認

--- web_test/urls.py
+++ web_test/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #1, URLに記述する文字列(今回の場合、「/web_test/」以降に続くもの)
     #2, 指定したURLにアクセスしたときに、どのようなビューをレンダリングするのか
     #3, URLを示すために名前を付ける(アプリケーションからの参照を楽にするため)
-    path('start', views.index, name='index'),
+    path('', views.index, name='index'),
     path("<str:name>", views.greet, name="greet"),
     path('brain', views.brain, name='brain')
 ]
